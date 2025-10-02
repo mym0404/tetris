a52048ccbb5f67b15f036e02d3cc3fae11d01116
MJ Studio <mym0404@gmail.com>
2025-10-02 15:08:26 +0900

    refactor: simplify scene structure and remove unused scenes
    - Remove Boot, MainMenu, GameOver scenes
    - Update Preloader to go directly to Game
    - Adjust game config (800x700 resolution)

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,19 +1,14 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
+import { AUTO, Game } from 'phaser';
 
-//  Find out more information about the Game Config at:
-//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
+  width: 800,
+  height: 700,
   parent: 'game-container',
-  backgroundColor: '#028af8',
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  backgroundColor: '#1a1a2e',
+  scene: [Preloader, MainGame],
 };
 
 const StartGame = (parent: string) => {
--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -5,36 +5,41 @@ export class Preloader extends Scene {
     super('Preloader');
   }
 
-  init() {
-    //  We loaded this image in our Boot Scene, so we can display it here
-    this.add.image(512, 384, 'background');
-
-    //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
-
-    //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
-
-    //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-    this.load.on('progress', (progress: number) => {
-      //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-      bar.width = 4 + 460 * progress;
-    });
-  }
-
   preload() {
-    //  Load the assets for the game - Replace with your own assets
-    this.load.setPath('assets');
+    // Create loading bar
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
 
-    this.load.image('logo', 'logo.png');
-    this.load.image('star', 'star.png');
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(width / 2 - 160, height / 2 - 25, 320, 50);
+
+    const loadingText = this.add
+      .text(width / 2, height / 2 - 50, 'Loading...', {
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        color: '#ffffff',
+      })
+      .setOrigin(0.5);
+
+    this.load.on('progress', (value: number) => {
+      progressBar.clear();
+      progressBar.fillStyle(0x00ff00, 1);
+      progressBar.fillRect(width / 2 - 150, height / 2 - 15, 300 * value, 30);
+    });
+
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+    });
+
+    // Load assets here if needed
+    this.load.setPath('assets');
   }
 
   create() {
-    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-    //  For example, you can define global animations here, so we can use them in other scenes.
-
-    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start('MainMenu');
+    this.scene.start('Game');
   }
 }
